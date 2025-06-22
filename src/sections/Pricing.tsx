93f5ad8dab8d2aef79496729f4e970fcf5f21a0c
beellionaire <nabeelabiyu@gmail.com>
2025-06-22 18:47:18 +0700

    add pricing page

---
+++ src/sections/Pricing.tsx
@@ -0,0 +1,148 @@
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+
+const pricingTiers = [
+  {
+    id: 1,
+    title: "Free",
+    monthlyPrice: 0,
+    buttonText: "Get started for free",
+    popular: false,
+    inverse: false,
+    features: [
+      "Up to 5 project members",
+      "Unlimited tasks and projects",
+      "2GB storage",
+      "Integrations",
+      "Basic support",
+    ],
+  },
+  {
+    id: 2,
+    title: "Pro",
+    monthlyPrice: 9,
+    buttonText: "Sign up now",
+    popular: true,
+    inverse: true,
+    features: [
+      "Up to 50 project members",
+      "Unlimited tasks and projects",
+      "50GB storage",
+      "Integrations",
+      "Priority support",
+      "Advanced support",
+      "Export support",
+    ],
+  },
+  {
+    id: 3,
+    title: "Business",
+    monthlyPrice: 19,
+    buttonText: "Sign up now",
+    popular: false,
+    inverse: false,
+    features: [
+      "Up to 5 project members",
+      "Unlimited tasks and projects",
+      "200GB storage",
+      "Integrations",
+      "Dedicated account manager",
+      "Custom fields",
+      "Advanced analytics",
+      "Export capabilities",
+      "API access",
+      "Advanced security features",
+    ],
+  },
+];
+
+export const Pricing = () => {
+  return (
+    <section className="py-24">
+      <div className="w-[80%] mx-auto flex flex-col items-center justify-center">
+        <div className="max-w-[900px] text-center">
+          <h2 className="title-section">Pricing Plan</h2>
+        </div>
+        <p className="description-section">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
+          explicabo? Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
+        <div className="flex flex-col gap-14 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+          {pricingTiers.map((tier) => {
+            return (
+              <div
+                key={tier.id}
+                className={twMerge(
+                  "p-10 rounded-3xl border border-[#000] shadow-[0_7px_14px_#EAEAEA max-w-lg w-full",
+                  tier.inverse === true && "border-black bg-black text-white/60"
+                )}
+              >
+                <div className="flex justify-between">
+                  <h3
+                    className={twMerge(
+                      "text-lg font-bold text-black/50",
+                      tier.inverse === true && "text-white/60"
+                    )}
+                  >
+                    {tier.title}
+                  </h3>
+                  {tier.popular === true && (
+                    <div className="inline-flex text-md px-4 py-1.5 rounded-xl border border-white/20">
+                      <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                        Popular
+                      </span>
+                    </div>
+                  )}
+                </div>
+                <div className="flex items-baseline gap-1 mt-[30px]">
+                  <span className="text-4xl tracking-tighter font-bold">
+                    $ {tier.monthlyPrice}
+                  </span>
+                  <span className="tracking-tight font-bold text-black/50">
+                    /month
+                  </span>
+                </div>
+                <button
+                  className={twMerge(
+                    "w-full bg-black text-white py-3 px-6 items-center justify-center rounded-lg border border-black hover:border-black hover:bg-transparent hover:text-black cursor-pointer transition-all duration-300 m-3",
+                    tier.inverse === true &&
+                      "bg-white text-black hover:text-white hover:border-white"
+                  )}
+                >
+                  {tier.buttonText}
+                </button>
+                <ul className="flex flex-col gap-5 mt-5">
+                  {tier.features.map((feature) => {
+                    return (
+                      <li
+                        key={feature}
+                        className="text-md flex items-center gap-4"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          className={twMerge(
+                            "w-6 h-6",
+                            tier.inverse ? "text-white" : "text-black"
+                          )}
+                        >
+                          <path
+                            fill="currentColor"
+                            d="M20.3367 8.43697L10.7367 18.037C10.6531 18.1209 10.5538 18.1875 10.4444 18.2329C10.335 18.2783 10.2177 18.3017 10.0992 18.3017C9.98079 18.3017 9.8635 18.2783 9.75411 18.2329C9.64471 18.1875 9.54535 18.1209 9.46174 18.037L5.26174 13.837C5.17802 13.7533 5.11162 13.6539 5.06631 13.5445C5.021 13.4351 4.99768 13.3179 4.99768 13.1995C4.99768 13.0811 5.021 12.9638 5.06631 12.8545C5.11162 12.7451 5.17802 12.6457 5.26174 12.562C5.34546 12.4783 5.44485 12.4118 5.55423 12.3665C5.66361 12.3212 5.78085 12.2979 5.89924 12.2979C6.01764 12.2979 6.13487 12.3212 6.24425 12.3665C6.35364 12.4118 6.45302 12.4783 6.53674 12.562L10.1 16.1252L19.0632 7.16348C19.2323 6.9944 19.4616 6.89941 19.7007 6.89941C19.9399 6.89941 20.1692 6.9944 20.3382 7.16348C20.5073 7.33255 20.6023 7.56187 20.6023 7.80098C20.6023 8.04008 20.5073 8.2694 20.3382 8.43848L20.3367 8.43697Z"
+                          />
+                        </svg>
+                        <span>{feature}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -4,14 +4,13 @@ const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] overflow-x-clip">
       <div className="w-[80%] mx-auto flex flex-col items-center justify-center">
-        
           <div className="tag">Boost Your Productivity</div>
           <div className="max-w-[900px] text-center">
-            <h2 className="p-4 text-3xl md:text-5xl md:leading-[60px] lg:text-6xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">
+            <h2 className="title-section">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae
             </h2>
           </div>
-          <p className="mt-5 sm:text-lg md:text-xl leading-[30px] tracking-tight text-[#010D3E] text-center max-w-[700px]">
+          <p className="description-section">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
             quos numquam eos ipsa, maiores consectetur!
           </p>
